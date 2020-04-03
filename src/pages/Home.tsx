0e0f0a9bd7afe8bb5ffa4d1d129ed88f47894974
ICHSAN <muhammad.nurichsan15@gmail.com>
2020-04-03 09:15:15 +0700

    firstUpload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,77 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, 
+          IonHeader, 
+          IonPage, 
+          IonTitle, 
+          IonToolbar, 
+          IonGrid, 
+          IonRow, 
+          IonCol, 
+          IonLabel, 
+          IonButton, 
+          IonItemDivider, 
+          IonInput } from '@ionic/react';
+import React , {useState, constructor} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  var[scorePlayer1, setScorePlayer1] = useState(0);
+  var[scorePlayer2, setScorePlayer2] = useState(0);
+    function fungsiMenaikkanScorePlayer1() {
+      scorePlayer1++
+      setScorePlayer1(scorePlayer1)
+    }
+    function fungsiMenurunkanScorePlayer1(){
+      if(scorePlayer1<=0)return
+      scorePlayer1--
+      setScorePlayer1(scorePlayer1)
+    }
+    function fungsiMenaikkanScorePlayer2() {
+      scorePlayer2++
+      setScorePlayer2(scorePlayer2)
+    }
+    function fungsiMenurunkanScorePlayer2(){
+      if(scorePlayer2<=0)return
+      scorePlayer2--
+      setScorePlayer2(scorePlayer2)
+    }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Badminton Scoring</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+            <IonRow>
+              <IonCol>
+                <div>Ichsan</div>
+                <div>
+                  <IonButton onClick={fungsiMenaikkanScorePlayer1}>+</IonButton>
+                </div>
+                <div>
+                  <IonInput value={String(scorePlayer1)}></IonInput>
+                </div>
+                <div>
+                  <IonButton onClick={fungsiMenurunkanScorePlayer1}>-</IonButton>
+                </div>
+              </IonCol>
+              <IonCol>
+                <div>Rothgar</div>
+                <div>
+                  <IonButton onClick={fungsiMenaikkanScorePlayer2}>+</IonButton>
+                </div>
+                <div>
+                  <IonInput value={String(scorePlayer2)}></IonInput>
+                </div>
+                <div>
+                  <IonButton onClick={fungsiMenurunkanScorePlayer2}>-</IonButton>
+                </div>
+              </IonCol>
+            </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
